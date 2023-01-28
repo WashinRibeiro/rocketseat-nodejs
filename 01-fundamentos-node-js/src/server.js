9367df407ced835144a950cb4eabc8e486fb96cb
Washington Ribeiro <washinuchiha@gmail.com>
2023-01-28 19:50:41 -0300

    estrutura da aplicação - conhecendo HTTP status codes

--- 01-fundamentos-node-js/src/server.js
+++ 01-fundamentos-node-js/src/server.js
@@ -1,26 +1,28 @@
-import http from 'node:http'
+import http from "node:http";
 
-
-const users = []
+const users = [];
 
 const server = http.createServer((req, res) => {
-    const { method, url } = req;
+  
+  const { method, url } = req;
 
-    if (method === 'GET' && url === '/users') {
-        return res
-            .setHeader('Content-Type', 'application/json')   
-            .end(JSON.stringify(users))
-    }
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
+  }
 
-    if (method === 'POST' && url === '/users') {
-        users.push({
-            id: 1,
-            name: 'John Doe',
-            email: 'john.doe@gmail.com',
-        })
-    }
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@gmail.com",
+    });
 
-    return res.end('Hello World')
-})
+    return res.writeHead(201).end('User created successfully');
+  }
 
-server.listen(3333)
+  return res.writeHead(404).end('Not Found');
+});
+
+server.listen(3333);
