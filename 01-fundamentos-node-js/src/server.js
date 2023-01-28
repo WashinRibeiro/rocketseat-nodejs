42abbbd5a4f5540b724190d65505c4f233f31f4a
Washington Ribeiro <washinuchiha@gmail.com>
2023-01-28 19:33:43 -0300

    estrutura da aplicação - salvando usuários em memória (headers)

--- 01-fundamentos-node-js/src/server.js
+++ 01-fundamentos-node-js/src/server.js
@@ -1,35 +1,23 @@
 import http from 'node:http'
 
-// Criar Usuário
-// Listagem de usuários
-// Edição de usuários
-// Remoção de usuários
 
-// Fundamentos de requisições
-// - HTTP 
-//      - Método HTTP
-//      - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET --> Buscar uma recurso do back-end
-// POST --> Criar uma recurso no back-end
-// PUT --> Editar ou atualizar uma recurso no back-end
-// PATCH --> Atualizar uma informa~çao única ou específica de um recurso do back-end
-// DELETE --> Deletar um recurso no back-end
-
-// GET /users => Buscando usuários do back-end
-// POST /users => Criando usuário no back-end
+const users = []
 
 const server = http.createServer((req, res) => {
     const { method, url } = req;
 
     if (method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuários')
+        return res
+            .setHeader('Content-Type', 'application/json')   
+            .end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
-        return res.end('Criação de usuário')
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'john.doe@gmail.com',
+        })
     }
 
     return res.end('Hello World')
